b45288890dfae5af4b007d92fa5c4e8fa97fdef0
wide-aze <wide-aze@student.42.fr>
2016-04-28 19:52:24 +0200

    updated dr_quine

--- dr_quine/Grace/Grace.c
+++ dr_quine/Grace/Grace.c
@@ -3,6 +3,10 @@ J'aime les poneys
 */
 #include <stdio.h>
 
-#define FT(x) int main(){ char eol='\n', qt='"', esc='\\'; char* str="/*%cJ'aime les poneys%c*/%c#include <stdio.h>%c%c#define FT(x) int main(){ char eol='%cn', qt='%c', esc='%c%c'; char* str=%c%s%c; FILE *fd=fopen(%cGrace_kid.c%c,%cw%c); if (fd) { fprintf(fd, str, eol, eol, eol, eol, eol, esc, qt, esc, esc, qt, str, qt, qt, qt, qt, qt, qt, esc, qt, eol, eol, eol); fclose(fd); } else printf(%cerror%cn%c); return (x); }%c%cFT(0)%c"; FILE *fd=fopen("Grace_kid.c","w"); if (fd) { fprintf(fd, str, eol, eol, eol, eol, eol, esc, qt, esc, esc, qt, str, qt, qt, qt, qt, qt, qt, esc, qt, eol, eol, eol); fclose(fd); } else printf("error\n"); return (x); }
+#define zero 0
 
-FT(0)
+#define FCLSE(a) fclose(a)
+
+#define FT(x) int main(){ char eol='\n', qt='"', esc='\\'; char* str="/*%cJ'aime les poneys%c*/%c#include <stdio.h>%c%c#define zero 0%c%c#define FCLSE(a) fclose(a)%c%c#define FT(x) int main(){ char eol='%cn', qt='%c', esc='%c%c'; char* str=%c%s%c; FILE *fd=fopen(%cGrace_kid.c%c,%cw%c); if (fd) { fprintf(fd, str, eol, eol, eol, eol, eol, eol, eol, eol, eol, esc, qt, esc, esc, qt, str, qt, qt, qt, qt, qt, qt, esc, qt, eol, eol, eol); FCLSE(fd); } else printf(%cerror%cn%c); return (x); }%c%cFT(zero)%c"; FILE *fd=fopen("Grace_kid.c","w"); if (fd) { fprintf(fd, str, eol, eol, eol, eol, eol, eol, eol, eol, eol, esc, qt, esc, esc, qt, str, qt, qt, qt, qt, qt, qt, esc, qt, eol, eol, eol); FCLSE(fd); } else printf("error\n"); return (x); }
+
+FT(zero)
